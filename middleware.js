3e44d9f93bce22b59e4ceea5971d67b85f378ecc
jingyi <jm624461806@outlook.com>
2021-08-20 16:34:42 -0700

    minor changes

--- middleware.js
+++ middleware.js
@@ -48,7 +48,7 @@ module.exports.validateReview = (req, res, next) => {
 
 module.exports.isReviewAuthor = async (req, res, next) => {
   const {id, reviewId} = req.params;
-  const review = await Review.findById(reviewID);
+  const review = await Review.findById(reviewId);
   if(!review.author.equals(req.user._id)) {
     req.flash('error', 'You do not have permission!');
     return res.redirect(`/campgrounds/${id}`);
